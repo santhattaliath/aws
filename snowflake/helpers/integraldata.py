2f258081ee66bf4b725b63020ed0b54bc245f03b
sandeep thattaliath <sandeep.thattaliath.1982@gmail.com>
2022-01-11 22:43:53 +0530

    Add files via upload

---
+++ snowflake/helpers/integraldata.py
@@ -0,0 +1,49 @@
+import pyodbc, pandas, sys, os
+import logging
+from datetime import datetime
+import boto3
+
+logFormat = '%(asctime)s %(message)s'
+logging.basicConfig(filename='process.log', format=logFormat, level=logging.INFO)
+# for folder name
+runDay = datetime.now().strftime('%d_%B_%Y')
+# for subfolder name with timestamp
+# folder and object names will be in lowercase
+runTime = datetime.now().strftime('%d_%B_%Y_%H_%M_%S').lower()
+
+try:
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 13 for SQL Server};SERVER=xx.xx.xx.xx;DATABASE=xx;UID=xx;PWD=xx')
+    if conn is not None:
+        logging.info('Connection established....')
+except:
+    print('error {}'.format(str(sys.exc_info()[0])))
+    sys.exit()
+
+tables = ['chdrpf', 'riskpf','descpf','cmovpf','ltrnpf','cheqpf']
+
+df = pandas.DataFrame()
+
+region = 'ap-southeast-1'
+# create s3 client
+s3Client = boto3.client('s3', aws_access_key_id='xxx',
+                        aws_secret_access_key='xxx')
+# create new folder for the day
+# check if bucketName exists for the day
+bucketName = 'integralba'
+folderName = 'integralaws_daily_feed_{}'.format(runDay).lower()
+logging.info('Creating folder {} under bucket {}'.format(bucketName, folderName))
+#s3Client.put_object(Bucket=bucketName, Key='{}'.format(folderName))
+
+for table in tables:
+    logging.info('Processing table {}'.format(table))
+    df = None
+    sql = 'select * from VM1DTA.{}'.format(table)
+    df = pandas.read_sql(sql, conn)
+    df.fillna(0, inplace=True)
+    # data to csv after cleansing
+    fileName = '{}_{}.csv'.format(table, runTime)
+    df.to_csv(fileName, index=False)
+    s3Client.upload_file(fileName, bucketName, '{}/{}'.format(folderName, fileName))
+
+# create new folder for every run
